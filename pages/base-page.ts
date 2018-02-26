36ca16f7d2a041a6edaeb638479e6e7bcfb760e2
karusya <karina.zhd@gmail.com>
2018-02-26 12:01:02 +0200

    updating page objects

--- pages/base-page.ts
+++ pages/base-page.ts
@@ -1,9 +1,12 @@
 import { browser, element, By, $, $$ } from 'protractor'
+
+
 export class BasePage {
 
-    public upcomingMoviesButton = $('#navbar li:nth-child(1)')
-    public popularSeriesButton = $('#navbar li:nth-child(2)')
-    
+    public upcomingMoviesHeader = $('#navbar li:nth-child(1)')
+    public popularSeriesHeader = $('#navbar li:nth-child(2)')
+    public actionCategory = $('ul.nav.nav-stacked a:nth-child(1)')
+    public URL = browser.baseUrl
 
     
 
